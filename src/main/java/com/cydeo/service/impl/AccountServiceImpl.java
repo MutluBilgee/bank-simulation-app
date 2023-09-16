2b185595d641377b7b6f509618ef8423b7db4b55
MutluBilgee <mutlubilgee@gmail.com>
2023-09-16 16:35:23 +0200

    Account service is completed

--- src/main/java/com/cydeo/service/impl/AccountServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/AccountServiceImpl.java
@@ -33,6 +33,6 @@ public class AccountServiceImpl implements AccountService {
 
     @Override
     public List<Account> listAllAccount() {
-        return null;
+        return accountRepository.findAll();
     }
 }
