1f30764388dd03c12cb8d1dd243c8b5114eae3e6
MutluBilgee <mutlubilgee@gmail.com>
2023-09-23 16:50:20 +0200

    added sender and receiver accounts

--- src/main/java/com/cydeo/BankSimulationAppApplication.java
+++ src/main/java/com/cydeo/BankSimulationAppApplication.java
@@ -1,16 +1,33 @@
 package com.cydeo;
 
+import com.cydeo.enums.AccountType;
+import com.cydeo.model.Account;
+import com.cydeo.service.AccountService;
+import com.cydeo.service.TransactionService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
 @SpringBootApplication
 public class BankSimulationAppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BankSimulationAppApplication.class, args);
+       ApplicationContext container = SpringApplication.run(BankSimulationAppApplication.class, args);
+
+       //get account and transaction service beans
+        AccountService accountService = container.getBean(AccountService.class);
+        TransactionService transactionService = container.getBean(TransactionService.class);
+
+        //create 2 accounts sender and receiver
+        Account sender = accountService.createNewAccount(BigDecimal.valueOf(70), new Date(), AccountType.CHECKING, 1L);
+        Account receiver = accountService.createNewAccount(BigDecimal.valueOf(50), new Date(), AccountType.CHECKING, 2L);
+
+        accountService.listAllAccount().forEach(System.out::println);
     }
 
 }
