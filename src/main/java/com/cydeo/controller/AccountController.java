91e626fe1c81c9063739572f4319d6bb43ab15f4
MutluBilgee <mutlubilgee@gmail.com>
2023-09-23 19:02:47 +0200

    List all accounts

---
+++ src/main/java/com/cydeo/controller/AccountController.java
@@ -0,0 +1,25 @@
+package com.cydeo.controller;
+
+import com.cydeo.service.AccountService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AccountController {
+
+    private final AccountService accountService;
+
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @GetMapping("/index")
+    public String getIndexPage(Model model){
+
+        model.addAttribute("accountList",accountService.listAllAccount());
+        return "account/index";
+//templates is the root. No need to provide the path explicitly
+    }
+}
--- src/main/java/com/cydeo/model/Account.java
+++ src/main/java/com/cydeo/model/Account.java
@@ -1,13 +1,16 @@
 package com.cydeo.model;
 
 import com.cydeo.enums.AccountType;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.util.Date;
 import java.util.UUID;
-
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 @Builder
 public class Account {
